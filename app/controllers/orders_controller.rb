719a340e1c749cf0a4a967689589455502437358
rails <rails@rails.com>
2016-02-19 17:18:17 +0530

    order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,22 @@ class OrdersController < ApplicationController
       format.json { render json: OrdersDatatable.new(view_context) }
     end
   end
+
+  def new
+  	@order = Order.new
+  end
+
+  def create
+  	@order = Order.new(new_params.merge(:user_id => current_user.id, :status => "in process"))
+  	if @order.save
+  		redirect_to orders_path
+  	else
+  		render :action => 'new'
+  	end
+  end
+
+  private
+  def new_params
+  	params.require(:order).permit!
+  end
 end
